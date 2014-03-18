6eaede0de0b2905242764c272a42865c3ca76c74
rozzerhmq <hmq_2006@hotmail.com>
2014-03-18 17:16:50 -0400

    update with static files

--- server.js
+++ server.js
@@ -3,9 +3,46 @@
 var http = require('http');
 var fs = require('fs');
 var util = require('util');
+var mime = require('mime');
 
 var port = 3000;
 var counter = 0;
+var cache = {};
+
+var error404 = function(res){
+   res.writeHead(404, {"Content-Type": 'text/plain'});
+   res.end("Not Found!");
+}
+
+var serveFile = function(path, res){
+   fs.readFile(path, function(err, data){
+      if(err) error404(res);
+
+      var type = mime.lookup(path);
+      cache[path] = {data: data, type: type};
+      res.writeHead(200, {'Content-Type': type});
+      res.end(data);
+   });
+}
+
+var serveStatic = function(req, res){
+   var fullPath = "";
+
+   if(req.url === '/'){
+      fullPath = __dirname + '/asset/html/index.html';
+   } else {
+      fullPath = __dirname + req.url;
+   }
+
+   if(cache[fullPath]){
+      var data = cache[fullPath];
+      res.writeHead(200, {'Content-Type': data.type});
+      res.end(data.data);
+   } else {
+      serveFile(fullPath, res);
+   }
+}
+
 http.createServer(function(req, res){
    var ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
@@ -22,8 +59,8 @@ http.createServer(function(req, res){
    });
 
    console.log(++counter);
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello,world!');
+
+   serveStatic(req, res);
 }).listen(port);
 
 console.log("Started http server, listening on port " + port);
